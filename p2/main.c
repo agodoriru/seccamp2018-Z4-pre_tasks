0d5cd051cf4a7e25173ae2865c7b31c4c07eabc7
agodoriru_notePC <ag0d0r1rv@gmail.com>
2018-08-12 17:19:39 +0900

    [add]

--- p2/main.c
+++ p2/main.c
@@ -59,6 +59,10 @@ char *get_src_ip(struct iphdr *iphdr);
 
 char *get_ip_protocol(struct iphdr *iphdr);
 
+void input_filter_info();
+void output_filter_info();
+int check_packet(const char *src_ip,const char *dest_ip,const char *proto,const char *src_port,const char **dest_port);
+
 // in sample program ////////////////////////////////
 
 struct pcap_file_hdr {
@@ -103,6 +107,13 @@ void write_packet(FILE* fp, const void* pkt, size_t len) {
 
 ////////////////////////////////////////////////////
 
+char filter_dest_ip[256];
+char filter_source_ip[256];
+char filter_protocol[256];
+char filter_dest_port[256];
+char filter_source_port[256];
+
+
 int main(int argc, char const *argv[]){
 
     u_char *pkt;
@@ -114,32 +125,10 @@ int main(int argc, char const *argv[]){
     	return (-1);
     }
 
-	u_char *filter_dest_ip[256];
-    u_char *filter_source_ip[256];
-	u_char *filter_protocol[256];
-	int filter_dest_port;
-	int filter_source_port;
-
-    fprintf(stdout, "input filter dest ip:");
-    scanf("%s",&filter_dest_ip);
-    fprintf(stdout, "input filter source ip:");
-    scanf("%s",&filter_source_ip);
-    fprintf(stdout, "input filter protocol:");
-    scanf("%s",&filter_protocol);
-    fprintf(stdout, "input filter dest port:");
-    scanf("%d",&filter_dest_port);
-    fprintf(stdout, "input filter source port:");
-    scanf("%d",&filter_source_port);
-
-    fprintf(stdout, " ======================= filter info =======================\n");
-    fprintf(stdout, " * filter_dest_ip:%s\n", filter_dest_ip);
-    fprintf(stdout, " * filter_source_ip:%s\n", filter_source_ip);
-    fprintf(stdout, " * filter_protocol:%s\n", filter_protocol);
-    fprintf(stdout, " * filter_dest_port:%u\n", filter_dest_port);
-    fprintf(stdout, " * filter_source_port:%u\n", filter_source_port);
-    fprintf(stdout, " ============================================================\n");
-
-    return 0;
+    input_filter_info();
+    output_filter_info();
+    // exit(0)
+ 
 
 	pcap_t *handle = pcap_open_offline_with_tstamp_precision(argv[1],PCAP_TSTAMP_PRECISION_NANO, errbuf);
 	if (handle == NULL) {
@@ -149,14 +138,18 @@ int main(int argc, char const *argv[]){
 
 	int count=0;
 
-	// FILE *fp=fopen("output.pcap","wb+");
-
+	FILE *fp=fopen("output.pcap","wb+");
+	write_filehdr(fp);
+	
 	while((pkt = pcap_next(handle, &pkthdr))){
 			count++;
 			fprintf(stdout, "==========================================================\n\n");
 			fprintf(stdout,"No.%d\n", count);
 			fprintf(stdout,"packet length : %d byte\n\n", pkthdr.caplen);
-			analyze_Packet(pkt,pkthdr.caplen);
+			if(analyze_Packet(pkt,pkthdr.caplen)==1){
+				sleep(5);
+				write_packet(fp, pkt, sizeof(pkt));
+			}
 	}
 
 	pcap_close(handle);
@@ -165,37 +158,10 @@ int main(int argc, char const *argv[]){
 }
 
 int analyze_Packet(u_char *data,int size){
-	
+
 	u_char *ptr;
 	int lest;
 	struct ether_header *eh;
-
-	ptr=data;
-	lest=size;
-
-	if(lest<sizeof(struct ether_header)){
-		fprintf(stderr, "lest(%d)<sizeof(struct ether_header)\n",lest );
-		return (-1);
-
-	}
-
-	eh=(struct ether_header *)ptr;
-	ptr+=sizeof(struct ether_header);
-	lest-=sizeof(struct ether_header);
-
-	if(ntohs(eh->ether_type)==ETHERTYPE_IP){
-		fprintf(stdout, "analyzing ip\n" );
-		analyze_IP(ptr,lest);
-	}
-
-	return 0;
-
-}
-
-int analyze_IP(u_char *data,int size){
-
-	u_char *ptr;
-	int lest;
 	struct iphdr *iphdr;
 
 	u_char *option;
@@ -203,6 +169,22 @@ int analyze_IP(u_char *data,int size){
 
 	ptr=data;
 	lest=size;
+
+	if(lest<sizeof(struct ether_header)){
+		fprintf(stderr, "lest(%d)<sizeof(struct ether_header)\n",lest );
+		return (-1);
+	}
+
+	eh=(struct ether_header *)ptr;
+	ptr+=sizeof(struct ether_header);
+	lest-=sizeof(struct ether_header);
+
+	if(ntohs(eh->ether_type)!=ETHERTYPE_IP){
+		// fprintf(stdout, "analyzing ip\n" );
+		// analyze_IP(ptr,lest);
+		
+	}
+	fprintf(stdout, "analyze ip\n");
 
 	if(lest<sizeof(struct iphdr)){
 		fprintf(stderr, "error\n");
@@ -233,27 +215,26 @@ int analyze_IP(u_char *data,int size){
 
 	if(iphdr->protocol==IPPROTO_TCP){
 
-		// print_IP_header(iphdr,stdout);
-
-		//checksum
-		fprintf(stdout, "==== port info ====\n");
-		fprintf(stdout,"src port:%u\n",get_tcp_dest_port(ptr));
-		fprintf(stdout,"dest port:%u\n",get_tcp_src_port(ptr));
+		output_filter_info();
+		
 		fprintf(stdout, "==== IP info ====\n");
-		fprintf(stdout, "src ip:%s\n", get_dest_ip(iphdr));
-		fprintf(stdout, "dest ip:%s\n", get_src_ip(iphdr));
+		fprintf(stdout, "src ip:%s\n", get_src_ip(iphdr));
+		fprintf(stdout, "dest ip:%s\n", get_dest_ip(iphdr));
 		fprintf(stdout, "ip protocol:%s\n", get_ip_protocol(iphdr));
+		fprintf(stdout, "==== port info ====\n");
+		fprintf(stdout,"src port:%u\n",get_tcp_src_port(ptr));
+		fprintf(stdout,"dest port:%u\n",get_tcp_dest_port(ptr));
+		
 		fprintf(stdout, "\n");
+		fprintf(stdout, "dest ip:%s\n", get_dest_ip(iphdr));
 
 
+		return(check_packet(get_src_ip(iphdr),get_dest_ip(iphdr),get_ip_protocol(iphdr),
+			get_tcp_src_port(ptr),get_tcp_dest_port(ptr)));
 
-
+		
 	}else if(iphdr->protocol==IPPROTO_UDP){
 	
-		// print_IP_header(iphdr,stdout);
-
-
-		// analyze_UDP(ptr,lest);
 		int packet_dest_port=get_udp_dest_port(ptr);
 		int packet_src_port=get_udp_dest_port(ptr);
 
@@ -338,18 +319,23 @@ char *get_ip_protocol(struct iphdr *iphdr){
     }else{
         // return "undifined";
     }
-
-    
 }
 
 char *get_dest_ip(struct iphdr *iphdr){
-	char buff[2048];
-	return(IP_address_int_to_IP_address_str(iphdr->saddr,buff,sizeof(buff)));
+	char buff_1[65535];
+	return(IP_address_int_to_IP_address_str(iphdr->daddr,buff_1,sizeof(buff_1)));
 }
 
 char *get_src_ip(struct iphdr *iphdr){
-	char buff[2048];
-	return(IP_address_int_to_IP_address_str(iphdr->daddr,buff,sizeof(buff)));
+	char buff_2[65535];
+	return(IP_address_int_to_IP_address_str(iphdr->saddr,buff_2,sizeof(buff_2)));
+}
+
+char *IP_address_int_to_IP_address_str(u_int32_t ip,char *buff,socklen_t size){
+	struct in_addr *addr;
+	addr=(struct in_addr *)&ip;
+	inet_ntop(AF_INET,addr,buff,size);
+	return(buff);
 }
 
 char *MACaddress_int_to_str(u_char *hwaddr,char *buff,socklen_t size){
@@ -358,9 +344,112 @@ char *MACaddress_int_to_str(u_char *hwaddr,char *buff,socklen_t size){
 	return(buff);
 }
 
-char *IP_address_int_to_IP_address_str(u_int32_t ip,char *buff,socklen_t size){
-	struct in_addr *addr;
-	addr=(struct in_addr *)&ip;
-	inet_ntop(AF_INET,addr,buff,size);
-	return(buff);
+
+
+void input_filter_info(){
+	fprintf(stdout, "input filter dest ip:");
+    scanf("%s",&filter_dest_ip);
+    fprintf(stdout, "input filter source ip:");
+    scanf("%s",&filter_source_ip);
+    fprintf(stdout, "input filter protocol:");
+    scanf("%s",&filter_protocol);
+    fprintf(stdout, "input filter dest port:");
+    scanf("%s",&filter_dest_port);
+    fprintf(stdout, "input filter source port:");
+    scanf("%s",&filter_source_port);
+}
+
+void output_filter_info(){
+    fprintf(stdout, " ======================= filter info =======================\n");
+    fprintf(stdout, " * filter_dest_ip:%s\n", filter_dest_ip);
+    fprintf(stdout, " * filter_source_ip:%s\n", filter_source_ip);
+    fprintf(stdout, " * filter_protocol:%s\n", filter_protocol);
+    fprintf(stdout, " * filter_dest_port:%s\n", filter_dest_port);
+    fprintf(stdout, " * filter_source_port:%s\n", filter_source_port);
+    fprintf(stdout, " ============================================================\n");
+    // exit(0);
+}
+
+int check_packet(const char *src_ip,const char *dest_ip,const char *proto,const char *src_port,const char **dest_port){
+		const char *any="any";
+		int chech_packet_arr[5]={0};
+		int check_count=0;
+
+			fprintf(stdout, "in check packet func \n" );
+			fprintf(stdout, "src ip          :%s\n",src_ip );
+			fprintf(stdout, "filter source ip:%s\n",filter_source_ip );
+			fprintf(stdout, "dest ip         :%s\n",dest_ip );
+			fprintf(stdout, "filter dest ip  :%s\n",filter_dest_ip );
+
+		if(strcmp(src_ip,filter_source_ip)==0||strcmp(filter_source_ip,any)==0){
+
+		
+			fprintf(stdout, "source IP:bingo\n");
+			chech_packet_arr[1]=1;
+
+		}else{
+			fprintf(stdout, "source IP:miss\n" );
+		}
+
+		if(strcmp(dest_ip,filter_dest_ip)==0||strcmp(filter_dest_ip,any)==0){
+		
+
+			fprintf(stdout, "destination IP:bingo\n");
+			chech_packet_arr[0]=1;
+		}else{
+			fprintf(stdout, "destination IP:miss\n" );
+		}
+		
+		if(strcmp(proto,filter_protocol)==0||strcmp(filter_protocol,any)==0){
+
+			fprintf(stdout, "ip protocol:bingo\n");
+			chech_packet_arr[2]=1;
+
+		}else{
+			fprintf(stdout, "IP protocol:miss\n" );
+		}
+
+		char get_tcp_dest_port_str[256];
+		char get_tcp_src_port_str[256];
+
+		snprintf(get_tcp_dest_port_str,256,"%d",dest_port);
+		snprintf(get_tcp_src_port_str,256,"%d",src_port);
+
+		// fprintf(stdout, "%s\n", get_tcp_dest_port_str);
+		// fprintf(stdout, "%s\n", get_tcp_src_port_str);
+
+		if(strcmp(get_tcp_src_port_str,filter_source_port)==0||strcmp(filter_source_port,any)==0){
+			fprintf(stdout, "source port:bingo\n");
+			chech_packet_arr[4]=1;
+		}else{
+			fprintf(stdout, "source port:miss\n" );
+		}
+
+
+		if(strcmp(get_tcp_dest_port_str,filter_dest_port)==0||strcmp(filter_dest_port,any)==0){
+			fprintf(stdout, "destination port:bingo\n");
+			chech_packet_arr[3]=1;
+
+		}else{
+			fprintf(stdout, "destination port:miss\n" );
+		}
+
+
+		int i;
+		for(i=0;i<5;i++){
+			if(chech_packet_arr[i]==1){
+				check_count++;
+			}else{
+
+			}
+		}
+
+			fprintf(stdout, "%d\n",check_count );
+
+		if(check_count==4){
+			sleep(5);
+			return 1;
+		}else{
+			return 0;
+		}
 }
