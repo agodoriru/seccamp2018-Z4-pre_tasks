c74d6e4a399bf50d40d9fd095be571627ed22bef
agodoriru_notePC <ag0d0r1rv@gmail.com>
2018-08-16 12:20:57 +0900

    [add]計測機能の実装

--- p2/main.c
+++ p2/main.c
@@ -34,6 +34,7 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <stdlib.h>
+#include "rdtsc.h"
 
 int analyze_ICMP(u_char * data, int size);
 bool analyze_Packet(const u_char * data, bpf_u_int32 size);
@@ -159,7 +160,10 @@ int main(int argc __attribute__((unused)), char const *argv[])
 	FILE *fp = fopen("output.pcap", "wb");
 	write_filehdr(fp);
 	output_filter_info();
-	
+
+	unsigned long long cnt_start, cnt_end;
+        cnt_start = rdtsc();
+
 	while ((pkt = pcap_next(handle, &pkthdr))) {
 		count++;
 		// fprintf(logfile, "==========================================================\n\n");
@@ -170,6 +174,8 @@ int main(int argc __attribute__((unused)), char const *argv[])
 			write_packet(fp, pkt, pkthdr.caplen);
 		}
 	}
+	cnt_end = rdtsc();
+        printf("rdtsc:%llu\n", cnt_end-cnt_start);
 	pcap_close(handle);
 	printf("match:%d\n", match);
 	printf("count:%d\n", count);
